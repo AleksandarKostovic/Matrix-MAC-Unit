56b4c5786b58449ee6684672f18419544851395a
Alexandar Kostovic <alexandar.kostovic@gmail.com>
2018-09-22 16:33:24 +0200

    Update matrix-mac-unit.v

--- matrix-mac-unit.v
+++ matrix-mac-unit.v
@@ -3,10 +3,11 @@
 //   Matrix Multiply and Accumulate unit     
 //===========================================
 
-module matrix_mac_unit 
-#(parameter DATA_WITDH = 8)(
+module matrix_mac_unit #(
+    parameter DATA_WITDH = 8
+)(
     clk,                 //|<input
     rst                  //|<input
-    );
+);
 
 endmodule
