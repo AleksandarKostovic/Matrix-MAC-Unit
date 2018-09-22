5451225fa1b4048f51c2507648f3cc18ce2fb3c0
Alexandar Kostovic <alexandar.kostovic@gmail.com>
2018-09-22 11:42:02 +0200

    Update matrix-mac-unit.v

--- matrix-mac-unit.v
+++ matrix-mac-unit.v
@@ -1,3 +1,8 @@
+/////////////////////////////////////////////
+// Copyright (c) Aleksandar Kostovic 2018  //
+//   Matrix Multiply and Accumulate unit   //  
+/////////////////////////////////////////////
+
 module matrix_mac_unit(
     clock,
     reset
