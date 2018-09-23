2367174f17108176f08cdfeedb1d0d8bc7734996
Alexandar Kostovic <alexandar.kostovic@gmail.com>
2018-09-23 08:29:50 +0200

    Update matrix-mac-unit.v

--- matrix-mac-unit.v
+++ matrix-mac-unit.v
@@ -6,8 +6,8 @@
 module matrix_mac_unit #(
     parameter integer DATA_WITDH = 8
 )( //ports
-    clk,          //|<input
-    rst           //|<input
+    clk,              //|<input
+    rst               //|<input
 );
 
 endmodule
