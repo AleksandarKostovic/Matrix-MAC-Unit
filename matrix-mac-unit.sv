711b33d55bd987c2485f0c1e0d8ead6961e47ca2
Alexandar Kostovic <alexandar.kostovic@gmail.com>
2018-09-25 08:03:34 +0200

    Update matrix-mac-unit.v

--- matrix-mac-unit.sv
+++ matrix-mac-unit.sv
@@ -7,26 +7,18 @@
 
 module matrix_mac_unit #(
    parameter DATA_WIDTH = 8
-)( //ports
-    clock,         //|<i
-    reset,         //|<i
-    enable,        //|<i
-    clear,         //|<i
-    matrix_1,      //|<i
-    matrix_2,      //|<i
-    result         //|>o 
-);
-
-//inputs
-   input [DATA_WIDTH-1:0]                   clock;
-   input [DATA_WIDTH-1:0]                   reset;
-   input [DATA_WIDTH-1:0]                  enable;
-   input [DATA_WIDTH-1:0]                   clear;
-   input [DATA_WIDTH-1:0]     matrix_1 [0:3][0:3];
-   input [DATA_WIDTH-1:0]     matrix_2 [0:3][0:3];
+)(
+   //inputs
+   input [DATA_WIDTH-1:0]                   clock,
+   input [DATA_WIDTH-1:0]                   reset,
+   input [DATA_WIDTH-1:0]                  enable,
+   input [DATA_WIDTH-1:0]                   clear,
+   input [DATA_WIDTH-1:0]     matrix_1 [0:3][0:3],
+   input [DATA_WIDTH-1:0]     matrix_2 [0:3][0:3],
 
 //outputs
-   output [DATA_WIDTH-1:0]      result [0:3][0:3];
+   output [DATA_WIDTH-1:0]      result [0:3][0:3]
+);
 
 //logic
    logic [DATA_WIDTH-1:0]  accumulator [0:3][0:3];
@@ -46,5 +38,6 @@ module matrix_mac_unit #(
     end
    
 //combinational logic
+assign result = enable ? matrix_1 * matrix_2 + accumulator : accumulator;
 
 endmodule
