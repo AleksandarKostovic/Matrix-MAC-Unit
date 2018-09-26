361997d1396612916efa0a53b94c353bbe757249
AleksandarKostovic <alexandar.kostovic@gmail.com>
2018-09-26 21:31:49 +0200

    Update matrix-mac-unit.sv

--- matrix-mac-unit.sv
+++ matrix-mac-unit.sv
@@ -7,17 +7,15 @@
 
 module matrix_mac_unit #(
    parameter DATA_WIDTH = 8
-)(
-//inputs
+)(//ports
+   
    input clock,
    input reset,
    input enable,
    input clear,
-   input logic [DATA_WIDTH-1:0] matrix_1 [0:3][0:3],
-   input logic [DATA_WIDTH-1:0] matrix_2 [0:3][0:3],
-
-//outputs
-   output logic [DATA_WIDTH-1:0] result [0:3][0:3]
+   input  logic [DATA_WIDTH-1:0] matrix_1 [0:3][0:3],
+   input  logic [DATA_WIDTH-1:0] matrix_2 [0:3][0:3],
+   output logic [DATA_WIDTH-1:0] result   [0:3][0:3]
 );
 
 //logic
