7b0fa9ae43b6a34152ec921db930a210d4e83245
AleksandarKostovic <alexandar.kostovic@gmail.com>
2018-09-28 22:39:55 +0200

    Update matrix-mac-unit.sv

--- src/matrix-mac-unit.sv
+++ src/matrix-mac-unit.sv
@@ -7,20 +7,20 @@
 
 import mmac_pkg::*;
 
-module matrix_mac_unit)
+module matrix_mac_unit
 (//ports
    
    input  clock,
    input  reset,
    input  enable,
    input  clear,
-   input  logic [DATA_WIDTH-1:0] matrix_1 [0:3][0:3],
-   input  logic [DATA_WIDTH-1:0] matrix_2 [0:3][0:3],
-   output logic [DATA_WIDTH-1:0] result   [0:3][0:3]
+   input  logic [DATA_WIDTH-1:0] matrix_1 [0:M_SIZE-1][0:M_SIZE-1],
+   input  logic [DATA_WIDTH-1:0] matrix_2 [0:M_SIZE-1][0:M_SIZE-1],
+   output logic [DATA_WIDTH-1:0] result   [0:M_SIZE-1][0:M_SIZE-1]
 );
 
 //logic
-   logic [DATA_WIDTH-1:0] accumulator [0:3][0:3];
+   logic [DATA_WIDTH-1:0] accumulator [0:M_SIZE-1][0:M_SIZE-1];
     
 //sequential logic
     always_ff @(posedge clock) begin
