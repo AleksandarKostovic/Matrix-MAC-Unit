ea6737e5f95df0f78115f7e41a8c884ddcff315e
AleksandarKostovic <alexandar.kostovic@gmail.com>
2018-10-04 19:48:00 +0200

    Update matrix-mac-unit.sv

--- src/matrix-mac-unit.sv
+++ src/matrix-mac-unit.sv
@@ -40,3 +40,37 @@ module matrix_mac_unit
 assign result = enable ? matrix_1 * matrix_2 + accumulator : accumulator;
 
 endmodule
+
+
+module Mat_mult(A,B,Res);
+  
+    input [63:0] A;
+    input [63:0] B;
+    output [63:0] Res;
+    //internal variables    
+    reg [63:0] Res;
+    reg [7:0] A1 [0:3][0:3];
+    reg [7:0] B1 [0:3][0:3];
+    reg [7:0] Res1 [0:3][0:3]; 
+    int i,j,k;
+
+    always_ff @ (A or B)
+    begin
+    //Initialize the matrices-convert 1 D to 3D arrays
+        {A1[0][0],A1[0][1],A1[1][0],A1[1][1],A1[1][2],A1[2][1],A1[2][0],A1[0][2],A1[2][3],A1[3][2],A1[3][0],A1[0][3],A1[3][1],A1[1][3]} = A;
+        {B1[0][0],B1[0][1],B1[1][0],B1[1][1],B1[1][2],B1[2][1],B1[2][0],B1[0][2],B1[2][3],B1[3][2],B1[3][0],B1[0][3],B1[3][1],B1[1][3]} = B;
+        i = 0;
+        j = 0;
+        k = 0;
+        {Res1[0][0],Res1[0][1],Res1[1][0],Res1[1][1],Res1[1][2],Res1[2][1],Res1[2][0],Res1[0][2],Res1[2][3],Res1[3][2],Res1[3][0],Res1[0][3],Res1[3][1],Res1[1][3]} = 32'd0; //initialize to zeros.
+        //Matrix multiplication
+        for(i=0;i < 4;i=i+1)
+            for(j=0;j < 4;j=j+1)
+                for(k=0;k < 4;k=k+1)
+                    Res1[i][j] = Res1[i][j] + (A1[i][k] * B1[k][j]);
+        //final output assignment - 3D array to 1D array conversion.            
+        Res = {Res1[0][0],Res1[0][1],Res1[1][0],Res1[1][1],Res1[1][2],Res1[2][1],Res1[2][0],Res1[0][2],Res1[2][3],Res1[3][2],Res1[3][0],Res1[0][3],Res1[3][1],Res1[1][3]};            
+    end 
+
+
+endmodule
