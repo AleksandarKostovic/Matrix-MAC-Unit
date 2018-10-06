62903d0577d673616a42916ca4308d376d92c0e6
AleksandarKostovic <alexandar.kostovic@gmail.com>
2018-10-06 06:56:41 +0200

    Update mmac_pkg.sv

--- src/mmac_pkg.sv
+++ src/mmac_pkg.sv
@@ -5,7 +5,9 @@
 
 package mmac_pkg; //matrix-mac-package
   
- localparam DATA_WIDTH = 8; //data width of the module
+ localparam DATA_WIDTH = 64; //data width of the module
+  
+ localparam VAR_WIDTH = 8;  //data width of internal variables
  
  localparam M_SIZE = 4; //matrix size
   
